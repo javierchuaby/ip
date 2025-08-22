b71df07db1a80039a27010f0548f7f0b2cedbfe5
Javier Chua <161703315+javierchuaby@users.noreply.github.com>
2025-08-22 16:17:04 +0800

    added horizontal lines to output

--- src/main/java/MrMoon.java
+++ src/main/java/MrMoon.java
@@ -1,9 +1,14 @@
 public class MrMoon {
+    private static final int LINE_LENGTH = 80;
+    private static final String LINE = "_".repeat(LINE_LENGTH);
     public static void main(String[] args) {
+        System.out.println(LINE);
         System.out.println(
                 "Hello! I'm Mr Moon!\n" +
-                "What can I do for you?\n" +
-                "Bye. Hope to see you again soon!"
+                "What can I do for you?"
         );
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
