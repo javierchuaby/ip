eb82a6a4213d89d705a8ef39b54638ecb538498b
Javier Chua <161703315+javierchuaby@users.noreply.github.com>
2025-08-22 16:08:28 +0800

    Added greeting and changed name

---
+++ src/main/java/MrMoon.java
@@ -0,0 +1,9 @@
+public class MrMoon {
+    public static void main(String[] args) {
+        System.out.println(
+                "Hello! I'm Mr Moon!\n" +
+                "What can I do for you?\n" +
+                "Bye. Hope to see you again soon!"
+        );
+    }
+}
